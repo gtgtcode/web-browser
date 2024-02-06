1c355519669d316720c2444e942eceeab306d4aa
gtgtcode <glthomas526@gmail.com>
2024-02-06 16:33:59 -0500

    working on tab audio

--- renderer/components/tab-template.tsx
+++ renderer/components/tab-template.tsx
@@ -1,3 +1,4 @@
+import { Button } from "antd";
 import { MutableRefObject, useEffect, useState } from "react";
 
 interface PageControlsClass {
@@ -22,6 +23,7 @@ export default function TabTemplate(props: {
   const title = props.PageControls.getTitles();
   const [URLTitle, setURLTitle] = useState([]);
   const [pageURL, setPageURL] = useState("");
+  const [tabAudible, setTabAudible] = useState(false);
 
   useEffect(() => {
     const titles = props.PageControls.getTitles();
@@ -35,7 +37,7 @@ export default function TabTemplate(props: {
       props.webContainer.current[props.index].addEventListener(
         "media-started-playing",
         () => {
-          console.log("media playing on tab " + props.index);
+          setTabAudible(true);
         }
       );
 
@@ -48,12 +50,30 @@ export default function TabTemplate(props: {
     }
   }, []);
 
+  useEffect(() => {
+    if (props.webContainer && props.webContainer.current) {
+      props.webContainer.current[props.index].addEventListener(
+        "media-paused",
+        () => {
+          setTabAudible(false);
+        }
+      );
+
+      return () => {
+        props.webContainer.current[props.index].removeEventListener(
+          "media-paused",
+          null
+        );
+      };
+    }
+  }, []);
+
   return (
     <div
       className={
         props.activeTab === props.index
-          ? "bg-zinc-700 rounded-t-xl mt-1 pt-1 px-4 max-w-[200px] relative basis-80 overflow-hidden max-h-[40px] bg-gradient-to-l from-zinc-700"
-          : "hover:bg-blue-700 group rounded-t-xl mt-1 pt-1 px-4 max-w-[200px] relative basis-80 overflow-hidden max-h-[40px] bg-gradient-to-t to-20% from-zinc-800"
+          ? "bg-zinc-700 rounded-t-xl mt-1 pt-1 px-4 max-w-[240px] relative basis-80 overflow-hidden max-h-[40px] bg-gradient-to-l from-zinc-700"
+          : "hover:bg-blue-700 group rounded-t-xl mt-1 pt-1 px-4 max-w-[240px] relative basis-80 overflow-hidden max-h-[40px] bg-gradient-to-t to-20% from-zinc-800"
       }
       onClick={() => props.setActiveTab(props.index)}
     >
@@ -85,6 +105,82 @@ export default function TabTemplate(props: {
               " from-zinc-700 absolute w-full h-full to-50% top-0 left-0"
             }
           ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              " from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              " from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              " from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              +" from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              +" from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              +" from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              +" from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div
+            className={
+              (props.activeTab < props.index ? "bg-gradient-to-l" : "hidden") +
+              (props.activeTab - props.index === 0 ? " hidden " : " ") +
+              +" from-zinc-800 absolute w-full h-full to-50% top-0 left-0"
+            }
+          ></div>
+          <div className="tab-controls absolute right-0 top-1/2 -translate-y-1/2 invert pr-1">
+            <Button
+              className={
+                tabAudible ? "transition-all" : "opacity-0 transition-all"
+              }
+              type="text"
+              shape="circle"
+              size="small"
+            >
+              <img src="/images/volume-up.svg" className="mx-auto" width={15} />
+            </Button>
+
+            <Button type="text" shape="circle" size="small">
+              <img
+                src="/images/add.svg"
+                className="mx-auto rotate-45"
+                width={15}
+              />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
